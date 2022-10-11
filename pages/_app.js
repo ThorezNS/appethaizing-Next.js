7ab057859b0059bca5d0785b3fb339b252fbf3b7
Maciej_Niemiec <niemiec.maciej@gmail.com>
2022-10-11 17:57:43 +0200

    add metadata

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>appethaizing</title>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="appetizing! find your favorite recipes with detailed step-by-step instructions"
+        />
+        <meta name="author" content="Maciej Niemiec" />
+        <link rel="icon" href="fork_knife_spoon_icon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
