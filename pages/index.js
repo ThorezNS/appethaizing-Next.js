bdd48955880e2c83d9300ed7ab4005e91286c093
Maciej_Niemiec <niemiec.maciej@gmail.com>
2022-10-16 23:05:28 +0200

    display recipe titles using server side rendering

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,26 @@
-export default function Home() {
-  return <></>;
+// import Curiosity from '../components/atoms/Curiosity/Curiosity';
+import Title from '../components/atoms/Title/Title';
+import CardsContainer from '../components/organisms/CardsContainer/CardsContainer';
+// import RecipeCard from '../organisms/RecipeCard/RecipeCard';
+
+export default function Home({ response }) {
+  return (
+    <CardsContainer>
+      <Title title={`get your random recipe`} heading />
+      {response.recipes.map((recipe) => {
+        return <p key={recipe.id}>{recipe.title}</p>;
+      })}
+      {/* <Curiosity /> */}
+    </CardsContainer>
+  );
+}
+
+export async function getServerSideProps() {
+  const KEY = process.env.REACT_APP_API_KEY;
+  const res = await fetch(
+    `https://api.spoonacular.com/recipes/random?apiKey=${KEY}&number=5`
+  );
+  const response = await res.json();
+
+  return { props: { response } };
 }
