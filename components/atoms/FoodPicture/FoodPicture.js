67e58e3974a6b825cda4f1f7e8a5e441c36c63d5
Maciej_Niemiec <niemiec.maciej@gmail.com>
2022-10-17 20:27:06 +0200

    change the card's img tag to the Image Next component

--- components/atoms/FoodPicture/FoodPicture.js
+++ components/atoms/FoodPicture/FoodPicture.js
@@ -1,9 +1,10 @@
 import styles from './FoodPicture.module.css';
+import Image from 'next/image';
 
 const FoodPicture = ({ src, alt }) => {
   return (
     <div className={styles.imageWrapper}>
-      <img src={src} alt={alt} className={styles.image} />
+      <Image layout="fill" src={src} alt={alt} className={styles.image} />
     </div>
   );
 };
