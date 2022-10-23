320db96a10852a251cd0629f2599c1d155578306
Maciej_Niemiec <niemiec.maciej@gmail.com>
2022-10-23 11:02:16 +0200

    insert a backup image in case it is not loaded

--- components/atoms/FoodPicture/FoodPicture.js
+++ components/atoms/FoodPicture/FoodPicture.js
@@ -2,11 +2,13 @@ import styles from './FoodPicture.module.css';
 import Image from 'next/image';
 
 const FoodPicture = ({ src, alt, card, info }) => {
-  let component;
+  let component, isSrc;
+  isSrc = src ? src : '/backup_image.jpg';
+
   if (card) {
-    component = <Image layout="fill" src={src} alt={alt} />;
+    component = <Image layout="fill" src={isSrc} alt={alt} />;
   } else if (info) {
-    component = <Image width={556} height={370} src={src} alt={alt} />;
+    component = <Image width={556} height={370} src={isSrc} alt={alt} />;
   }
   return <div className={styles.imageWrapper}>{component}</div>;
 };
